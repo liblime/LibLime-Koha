4bdb473ab21ddb72af202a82b5b6fa1164afea7a
PTFS <koha-dev@ptfs.com>
2009-06-13 15:02:47 -0400

    Added date to the previous cardnumber list.

--- C4/Stats.pm
+++ C4/Stats.pm
@@ -125,14 +125,12 @@ sub GetPreviousCardnumbers {
   my $member = C4::Members::GetMember( $borrowernumber );
   my $cardnumber = $member->{'cardnumber'};
   
-  my $query = "SELECT DISTINCT(other) AS previous_cardnumber FROM statistics WHERE borrowernumber = ? AND other != ? AND other !='' ";
+  my $query = "SELECT DISTINCT(other) AS previous_cardnumber, DATE_FORMAT( datetime, '%m/%e/%Y') as previous_cardnumber_date FROM statistics WHERE borrowernumber = ? AND other != ? AND other !='' ";
   my $sth = $dbh->prepare( $query );
   $sth->execute( $borrowernumber, $cardnumber );
 
   my @results;
   while ( my $data = $sth->fetchrow_hashref ) {
-
-warn "Found Previous Number: " . $data->{'previous_cardnumber'};
     push( @results, $data );
   }
   $sth->finish;
