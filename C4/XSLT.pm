52a45cb48dbd597f0e6e15c3d00b5457d8a20fbc
David Birmingham <dbirmingham@ptfs.com>
2010-04-02 17:12:59 -0400

    Accounted for suppress status in XSLT display

--- C4/XSLT.pm
+++ C4/XSLT.pm
@@ -162,8 +162,9 @@ sub buildKohaItemsNamespace {
 
         my ( $transfertwhen, $transfertfrom, $transfertto ) = C4::Circulation::GetTransfers($item->{itemnumber});
         my ($restype,$reserves) = C4::Reserves::CheckReserves($item->{itemnumber});
-        if ( $itemtypes->{ $item->{itype} }->{notforloan} || $item->{notforloan} || $item->{onloan} || $item->{wthdrawn} || $item->{itemlost} || $item->{damaged} ||
+        if ( $itemtypes->{ $item->{itype} }->{notforloan} == 1 || $item->{notforloan} || $item->{onloan} || $item->{wthdrawn} || $item->{itemlost} || $item->{damaged} || $item->{suppress} ||
              (defined $transfertwhen && $transfertwhen ne '') || $item->{itemnotforloan} || $item->{reserve_status} eq "Attached" || $item->{reserve_status} eq "Reserved" || $item->{reserve_status} eq "Waiting" ) {
+            next if ($item->{suppress});
             if (($restype eq "Attached") || ($restype eq "Reserved") || ($restype eq "Waiting")){
                 $status = 'On hold';
             }
