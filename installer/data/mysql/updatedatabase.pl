a80cb6b3534f50e3bbb735fac802993e1bd70ef2
Clay Fouts <cfouts@ptfs.com>
2011-01-10 22:44:02 +0000

    Micro version update to 1.3.8

--- installer/data/mysql/updatedatabase.pl
+++ installer/data/mysql/updatedatabase.pl
@@ -4027,6 +4027,12 @@ if (C4::Context->preference("Version") < TransformToNum($DBversion)) {
     print "Upgrade to $DBversion done ( Added syspref EnableHoldExpiredNotice and EnableHoldCancelledNotice )\n";
 }
 
+$DBversion = '4.03.08.000';
+if (C4::Context->preference("Version") < TransformToNum($DBversion)) {
+    SetVersion ($DBversion);
+    print "Upgrade to $DBversion done ( Micro version update )\n";
+}
+
 printf "Database schema now up to date at version %s as of %s.\n", $DBversion, scalar localtime;
 
 =item DropAllForeignKeys($table)
