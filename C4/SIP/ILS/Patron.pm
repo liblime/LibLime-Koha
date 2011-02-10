10f4358e044de5f601b61f2cd0e5b1fc50e863f5
David Birmingham <dbirmingham@ptfs.com>
2011-02-10 17:05:45 -0500

    PT9501501:  SCLS SIP - replace noissuescharge with circ_block_threshold
    SIP was previously using the all-encompassing noissuescharge syspref to
    control whether a patron would be denied various transactions as a result
    of excessive fines.  This behavior was altered to be controlled by the
    circ_block_threshold value in each patron category in Koha.  This patch
    satisfies the new requirement as it pertains to the SIP module.

--- C4/SIP/ILS/Patron.pm
+++ C4/SIP/ILS/Patron.pm
@@ -298,20 +298,14 @@ sub inet_privileges {
 }
 
 sub fee_limit {
-    #my $self = shift;
+    my $self = shift;
 
-    ## Dave, this needs to read something like
-    ##
-    ##      my($self, $categorycode) = @_;
-    ##      my $fee      = 5;
-    ##      my $category = C4::Members::GetCatgoryInfo($categorycode);
-    ##      if (C4::Context->preference('UseGranularMaxFines')) {
-    ##         $fee = $category->{'circ_block_threshold'} || 5;
-    ##      }
-    ##      return $fee;
-    #return C4::Context->preference("noissuescharge") || 5;
-    die "noissuecharge syspref is deprecated, use UseGranularMaxFines syspref 
-      with circ_block_threshold by-patron category";
+    my $fee = 5;
+    my $category = C4::Members::GetCategoryInfo($self->{ptype});
+    if (C4::Context->preference('UseGranularMaxFines')) {
+      $fee = $category->{'circ_block_threshold'} || 5;
+    }
+    return $fee;
 }
 
 sub excessive_fees {
