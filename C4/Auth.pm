liblime/LibLime-Koha

commit 7ca50db48e966b969c05881432099140f5e33fe2

PTFS PT 1064319 warp speed cataloguing for partial barcode Also fixed: * suppress Perl warning on uninit val $session upon login * revert inequality checking in barcodedecode() * coerce numeric bitwise-AND for superlibrarian

C4/Auth.pm
@@ -608,8 +608,9 @@ sub checkauth {
             $userid   = $session->param('id');
             $sessiontype = $session->param('sessiontype');
         }
-   
-        if ( ($query->param('koha_login_context')) && ($query->param('userid') ne $session->param('id')) ) {
+
+        if ( ($query->param('koha_login_context')) 
+        && ($query->param('userid') ne ($session->param('id') // ''))  ) {
             #if a user enters an id ne to the id in the current session, we need to log them in...
             #first we need to clear the anonymous session...
             $debug and warn "query id = " . $query->param('userid') . " but session id = " . $session->param('id');
@@ -1412,7 +1413,7 @@ sub getuserflags {
     $sth->execute;
 
     while ( my ( $bit, $flag, $defaulton ) = $sth->fetchrow ) {
-        if ( ( $flags & ( 2**$bit ) ) || $defaulton ) {
+        if ( ( ($flags?1:0) & ( 2**$bit ) ) || $defaulton ) {
             $userflags->{$flag} = 1;
         }
         else {

C4/Circulation.pm
@@ -210,7 +210,7 @@ sub barcodedecode
             die "No library set and/or no branchcode passed to barcodedecode()";
         }
         my $padding = C4::Context->preference('itembarcodelength') - length($prefix) - length($filtered);
-        $filtered = $prefix . '0' x $padding . $filtered if ($padding > 0);
+        $filtered = $prefix . '0' x $padding . $filtered if ($padding >= 0);
     }
     return $filtered || $g{barcode};
 }

cataloguing/addbooks.pl
@@ -74,7 +74,8 @@ if ($query) {
    }
 
     # find results
-    my ( $error, $marcresults, $total_hits ) = SimpleSearch($query, $results_per_page * ($page - 1), $results_per_page);
+    my $offset = $results_per_page * ($page - 1);
+    my ( $error, $marcresults, $total_hits ) = SimpleSearch($query, $offset, $results_per_page);
 
     if ( defined $error ) {
         $template->param( error => $error );
@@ -92,7 +93,7 @@ if ($query) {
    foreach my $result(@newresults) {
       my(@barcodes) = split(/\s*\|\s*/,$$result{barcode});
       foreach my $i(0..$#barcodes) {
-         if ($barcodes[$i] eq $input->param('q')) { # exact search match on barcode
+         if ($barcodes[$i] eq $query) { # exact search match on barcode
             my @inums = split(/\s*\|\s*/, $$result{itemnumber});
             print $input->redirect('additem.pl?biblionumber='
             . $$result{biblionumber}.'&op=edititem&itemnumber='

commit eb764daefcd86203235758eb25b23a538b2c5448

Fix user permissions bug introduced in 7ca50db

C4/Auth.pm
@@ -1413,7 +1413,7 @@ sub getuserflags {
     $sth->execute;
 
     while ( my ( $bit, $flag, $defaulton ) = $sth->fetchrow ) {
-        if ( ( ($flags?1:0) & ( 2**$bit ) ) || $defaulton ) {
+        if ( ($flags & 2**$bit) || $defaulton ) {
             $userflags->{$flag} = 1;
         }
         else {