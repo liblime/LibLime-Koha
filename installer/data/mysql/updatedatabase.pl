e8f714eded647731641cb1893cef09e30cac8e65
Clay Fouts <cfouts@liblime.com>
2011-03-29 05:10:15 +0000

    Increment micro version number

--- installer/data/mysql/updatedatabase.pl
+++ installer/data/mysql/updatedatabase.pl
@@ -4567,6 +4567,12 @@ if (C4::Context->preference("Version") < TransformToNum($DBversion)) {
     print "Upgrade to $DBversion done ( Add authvals for facet searches )\n";
 }
 
+$DBversion = '4.03.19.000';
+if (C4::Context->preference("Version") < TransformToNum($DBversion)) {
+    SetVersion ($DBversion);
+    print "Upgrade to $DBversion done ( Micro version update to $DBversion )\n";
+}
+
 printf "Database schema now up to date at version %s as of %s.\n", $DBversion, scalar localtime;
 
 =item DropAllForeignKeys($table)
