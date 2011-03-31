5759a484eef411acb18436eedfc28d117ff134fc
Clay Fouts <cfouts@liblime.com>
2011-03-31 20:00:47 +0000

    [Fixes #11721523] unable to delete periodicals
    Using GET as the request method seemed to be mucking up the works.
    POST is somewhat more appropriate for this purpose, anyway.

--- periodicals/delete.pl
+++ periodicals/delete.pl
@@ -9,7 +9,7 @@ use C4::Auth;
 use C4::Output;
 use C4::View::Serials;
 
-my $query = new CGI;
+my $query = CGI->new();
 my ($template, $loggedinuser, $cookie) = 
     get_template_and_user({template_name => "periodicals/delete.tmpl",
                            query => $query,
