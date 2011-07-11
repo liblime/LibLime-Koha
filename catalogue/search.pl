8ec02ce72ba449c54444618cdd1f12be1aad8361
Clay Fouts <cfouts@liblime.com>
2011-07-11 23:22:11 +0000

    [delivers #14668229] Broken advanced search with branch groups
    Parentheses were missing from around branches list.

--- catalogue/search.pl
+++ catalogue/search.pl
@@ -429,9 +429,9 @@ if ($suggest_count) {
 # limits are use to limit to results to a pre-defined category such as branch or language
 my @limits;
 @limits = split("\0",$params->{'limit'}) if $params->{'limit'};
-
-if($params->{'multibranchlimit'}) {
-push @limits, join(" or ", map { "branch: $_ "}  @{GetBranchesInCategory($params->{'multibranchlimit'})}) ;
+if($params->{multibranchlimit}) {
+    my $branchlist = GetBranchesInCategory($params->{multibranchlimit});
+    push @limits, sprintf '(%s)', join(' or ', map { "branch: $_"} @{$branchlist});
 }
 
 my $available;
