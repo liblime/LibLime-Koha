ae6cf343f1b2a21016def72df627d84fb47eabcc
Clay Fouts <cfouts@liblime.com>
2011-08-15 15:44:44 -0700

    Schema version update: 4.9.0.1

--- kohaversion.pl
+++ kohaversion.pl
@@ -10,7 +10,7 @@
 use strict;
 
 sub kohaversion {
-    our $VERSION = '4.09.00.000';
+    our $VERSION = '4.09.00.001';
     # version needs to be set this way
     # so that it can be picked up by Makefile.PL
     # during install
