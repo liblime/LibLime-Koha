47be534972a4a75fb68c41797d3e9e39d8d6edfa
Clay Fouts <cfouts@liblime.com>
2012-03-26 13:30:26 -0700

    Write all params to error log in CatchErrors middleware

--- lib/Koha/Plack/CatchErrors.pm
+++ lib/Koha/Plack/CatchErrors.pm
@@ -3,16 +3,21 @@ use parent qw(Plack::Middleware);
 
 use Koha;
 use Plack::Util::Accessor qw(logger);
+use Plack::Request;
 use Try::Tiny;
+use Data::Dumper;
 
 sub call {
     my ($self, $env) = @_;
+
+    my $req = Plack::Request->new($env);
 
     my $retval = try {
         $self->app->($env);
     }
     catch {
-        my $error = "KOHA_PLACK_ERROR:$_".Data::Dumper::Dumper($env).Data::Dumper::Dumper(\%ENV);
+        my $eid = sprintf '%08x', rand(2**31);
+        my $error = "EXCEPTION($eid):$_".Dumper($env).Dumper($req->parameters());
         if (my $log = $self->logger) {
             $log->error($error);
         }
@@ -23,7 +28,7 @@ sub call {
         return [
             500,
             [],
-            ["Internal Server Error<br/>\n"]
+            ["Internal Server Error (support code $eid)\n"]
         ];
     };
 }
