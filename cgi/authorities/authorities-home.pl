4bd384a506f1b227d18efba01cc2e57cf0d26e0b
Ryan Higgins <ryan.rch@gmail.com>
2012-07-11 23:08:54 -0700

    Solr Authority search in staff interface.

--- cgi/authorities/authorities-home.pl
+++ cgi/authorities/authorities-home.pl
@@ -18,6 +18,7 @@
 # Suite 330, Boston, MA  02111-1307 USA
 
 use strict;
+use warnings;
 
 use CGI;
 use C4::Auth;
@@ -27,23 +28,40 @@ use C4::Context;
 use C4::Auth;
 use C4::Output;
 use C4::AuthoritiesMarc;
-use C4::Acquisition;
 use C4::Koha;    # XXX subfield_is_koha_internal_p
-use C4::Biblio;
+use Koha::Solr::Service;
+use Koha::Solr::Query;
+use Koha::Pager;
 
 my $query        = new CGI;
-my $op           = $query->param('op');
-my $authtypecode = $query->param('authtypecode');
+my $op           = $query->param('op') || '';
+my $authtypecode = $query->param('authtypecode') || '';
 my $dbh          = C4::Context->dbh;
 
-my $authid = $query->param('authid');
-my ( $template, $loggedinuser, $cookie );
+my $start = $query->param('start') // 0;
+my $authid    = $query->param('authid');
+my $template_file = ($query->param('index')) ? "auth_finder.tmpl" : "auth_search.tmpl";
+
+my ( $template, $loggedinuser, $cookie )= get_template_and_user(
+        {
+            template_name   => "authorities/$template_file",
+            query           => $query,
+            type            => 'intranet',
+            authnotrequired => 0,
+            flagsrequired   => { catalogue => 1 },
+            debug           => 1,
+        }
+    );
+my $resultsperpage;
 
 my $authtypes = getauthtypes;
 my @authtypesloop;
-foreach my $thisauthtype ( sort { $authtypes->{$a}{'authtypetext'} cmp $authtypes->{$b}{'authtypetext'} }
-    keys %$authtypes )
-{
+#$authtypecode = 'TOPIC_TERM' unless $authtypecode;
+
+foreach my $thisauthtype ( sort { $authtypes->{$a}{'authtypetext'} cmp $authtypes->{$b}{'authtypetext'} } keys %$authtypes ){
+#    next unless $thisauthtype; # There should be no default authority types.
+#    FIXME: This allows a frameworkless 'default' type.
+#    It also allows a search on any authority type.
     my $selected = 1 if $thisauthtype eq $authtypecode;
     my %row = (
         value        => $thisauthtype,
@@ -53,124 +71,63 @@ foreach my $thisauthtype ( sort { $authtypes->{$a}{'authtypetext'} cmp $authtype
     push @authtypesloop, \%row;
 }
 
-if ( $op eq "do_search" ) {
-    my @marclist  = $query->param('marclist');
-    my @and_or    = $query->param('and_or');
-    my @excluding = $query->param('excluding');
-    my @operator  = $query->param('operator');
-    my $orderby   = $query->param('orderby');
-    my @value     = $query->param('value');
-
-    my $startfrom      = $query->param('startfrom')      || 1;
-    my $resultsperpage = $query->param('resultsperpage') || 20;
-
-    my ( $results, $total ) =
-      SearchAuthorities( \@marclist, \@and_or, \@excluding, \@operator, \@value,
-        ( $startfrom - 1 ) * $resultsperpage,
-        $resultsperpage, $authtypecode, $orderby, {show_unlinked => 1} );
-#     use Data::Dumper; warn Data::Dumper::Dumper(@$results);
-    ( $template, $loggedinuser, $cookie ) = get_template_and_user(
-        {
-            template_name   => "authorities/searchresultlist.tmpl",
-            query           => $query,
-            type            => 'intranet',
-            authnotrequired => 0,
-            flagsrequired   => { catalogue => 1 },
-            debug           => 1,
-        }
-    );
-
-    my @field_data = ();
-
-    # we must get parameters once again. Because if there is a mainentry, it
-    # has been replaced by something else during the search, thus the links
-    # next/previous would not work anymore
-    my @marclist_ini = $query->param('marclist');
-    for ( my $i = 0 ; $i <= $#marclist ; $i++ ) {
-        if ($value[$i]){   
-          push @field_data, { term => "marclist",  val => $marclist_ini[$i] };
-          push @field_data, { term => "and_or",    val => $and_or[$i] };
-          push @field_data, { term => "excluding", val => $excluding[$i] };
-          push @field_data, { term => "operator",  val => $operator[$i] };
-          push @field_data, { term => "value",     val => $value[$i] };
-        }    
-    }
-
-    # construction of the url of each page
-    my $base_url =
-        'authorities-home.pl?'
-      . join( '&amp;', map { $_->{term} . '=' . $_->{val} } @field_data )
-      . '&amp;'
-      . join(
-        '&amp;',
-        map { $_->{term} . '=' . $_->{val} } (
-            { term => 'resultsperpage', val => $resultsperpage },
-            { term => 'type',           val => 'intranet' },
-            { term => 'op',             val => 'do_search' },
-            { term => 'authtypecode',   val => $authtypecode },
-            { term => 'orderby',        val => $orderby },
-        )
-      );
-
-    my $from = ( $startfrom - 1 ) * $resultsperpage + 1;
-    my $to;
-
-    if ( $total < $startfrom * $resultsperpage ) {
-        $to = $total;
-    }
-    else {
-        $to = $startfrom * $resultsperpage;
-    }
-
-    $template->param( result => $results ) if $results;
-
-    $template->param(
-        pagination_bar => pagination_bar(
-            $base_url,  int( $total / $resultsperpage ) + 1,
-            $startfrom, 'startfrom'
-        ),
-        total     => $total,
-        from      => $from,
-        to        => $to,
-        isEDITORS => $authtypecode eq 'EDITORS',
-    );
-
-}
-elsif ( $op eq "delete" ) {
-
+if ( $op eq "delete" ) {
     &DelAuthority( $authid, 1 );
 
-    ( $template, $loggedinuser, $cookie ) = get_template_and_user(
-        {
-            template_name   => "authorities/authorities-home.tmpl",
-            query           => $query,
-            type            => 'intranet',
-            authnotrequired => 0,
-            flagsrequired   => { catalogue => 1 },
-            debug           => 1,
-        }
+} elsif ( $op eq "do_search" ) {
+
+    my $idx = $query->param('idx');
+    my $q = $query->param('q');
+    my $sortby = $query->param('orderby');
+    if($query->param('operator') eq 'is'){
+        $q = '"'.$q.'"';
+    } elsif($query->param('operator') eq 'start'){
+        $q .= '*';
+    }
+    my $query_string = ($idx eq 'auth-heading')? 'auth-heading:' : 'auth-full:';
+    $query_string .= $q;
+    my $options = { 'sort' => $sortby };
+    $options->{fq} = "kauthtype_s:$authtypecode" if $authtypecode;
+    $options->{start} = $start if $start;
+
+    my $solr = Koha::Solr::Service->new();
+    my $solr_query = Koha::Solr::Query->new( {query => $query_string, options => $options, rtype => 'auth' } );
+
+    my $rs = $solr->search($solr_query->query,$solr_query->options);
+
+    my $resultset = ($rs->is_error) ? '' : $rs->content;
+    my $results = [];
+
+    for my $doc (@{$resultset->{response}->{docs}}){
+        my $record = MARC::Record->new_from_xml($doc->{marcxml},'utf8','MARC21'); 
+        my $summary = C4::AuthoritiesMarc::BuildSummary($record,undef,$authtypecode);
+        push @$results, {   summary => $summary,
+                            authid => $doc->{authid},
+                            authtype => $doc->{kauthtype_s},
+                            used => C4::AuthoritiesMarc::CountUsage($doc->{authid}),
+
+                        };
+    }
+    my $resultsperpage;
+    my $total = $resultset->{'response'}->{'numFound'};
+
+    my $pager = Koha::Pager->new({pageset => $rs->pageset, offset_param => 'start'});
+
+    $template->param(   result          => $results,
+                        orderby         => $sortby,
+                        total           => $total,
+                        authtypecode    => $authtypecode,
+                        q               => $q,
+                        pager           => $pager->tmpl_loop(),
+                        from            => $pager->first,
+                        to              => $pager->last(),
     );
 
-    # 	$template->param("statements" => \@statements,
-    # 						"nbstatements" => $nbstatements);
-}
-else {
-    ( $template, $loggedinuser, $cookie ) = get_template_and_user(
-        {
-            template_name   => "authorities/authorities-home.tmpl",
-            query           => $query,
-            type            => 'intranet',
-            authnotrequired => 0,
-            flagsrequired   => { catalogue => 1 },
-            debug           => 1,
-        }
-    );
-
 }
 
-$template->param(
-    authtypesloop => \@authtypesloop,
-);
+$template->param( authtypesloop => \@authtypesloop,
+                  index           => $query->param('index'),
+                );
 
-# Print the page
 output_html_with_http_headers $query, $cookie, $template->output;
+
