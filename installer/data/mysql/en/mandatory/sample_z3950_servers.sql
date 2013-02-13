848ff0f1ea190bc3b9e9bc0340dbdfba58594756
jwt01 <jwt01@kohatest.ec2.liblime.com>
2013-02-13 13:40:44 +0000

    [#43343485] Update LC z39.50 connection
    Update install connection information for Library of Congress z39.50
    server; also removed obsolete connection for Smithsonian (no longer
    functional).  Also moved from optional to mandatory install, to
    match practice and LAK code.

---
+++ installer/data/mysql/en/mandatory/sample_z3950_servers.sql
@@ -0,0 +1,4 @@
+INSERT INTO `z3950servers`
+(`host`, `port`, `db`, `userid`, `password`, `name`, `id`, `checked`, `rank`, `syntax`, `encoding`) VALUES 
+('lx2.loc.gov',210,'LCDB','','','LIBRARY OF CONGRESS',1,1,1,'USMARC','utf8'),
+('clio-db.cc.columbia.edu',7090,'voyager','','','COLUMBIA UNIVERSITY',6,0,0,'USMARC','MARC-8');
