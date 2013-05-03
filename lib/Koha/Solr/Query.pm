4aa99e6673fe06a5b1ba8977fd4ad1e5663ba4b4
Clay Fouts <cfouts@liblime.com>
2013-05-03 08:30:21 -0700

    Revert "[#44806147] Force AND-ing of grouped search terms"
    This reverts commit e7475c9ee46a19f7e9aa694c42e0f1ef766c29ef.

--- lib/Koha/Solr/Query.pm
+++ lib/Koha/Solr/Query.pm
@@ -156,7 +156,7 @@ sub _build_query_from_cgi{
 
             elsif( $q !~ /"/ && $q !~ /\(|\)/ && $q =~ /\S+\s+\S+/ ) {
                 # Add grouping for this field if not quoted and multiple terms.
-                $q = sprintf( '(%s)', join(' AND ', split(/\s+/, $q)) );
+                $q = "($q)";
             }
             $query .= "$idx:$q";
         }
